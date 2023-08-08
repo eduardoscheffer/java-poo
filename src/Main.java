60295d91410d13320c77960bed900eb89f333b85
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-08 13:23:31 -0300

    entrada de dados na mesma linha

--- src/Main.java
+++ src/Main.java
@@ -3,47 +3,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault((Locale.US));
         Scanner sc = new Scanner(System.in);
 
-        String product1 = "Computer";
-        String product2 = "Office desk";
-        int age = 30;
-        int code = 5290;
-        char gender = 'F';
-        double price1 = 2100.0;
-        double price2 = 650.50;
-        double measure = 53.234567;
+        String nome;
+        int idadeNome, idadePai, idadeMae;
+        nome = sc.nextLine();
+        idadeNome = sc.nextInt();
+        idadePai = sc.nextInt();
+        idadeMae = sc.nextInt();
+        System.out.printf(String.format("Nome: %s, Idade: $d, Idade Pai: %d, IdadeMae: %d",
+                nome, idadeNome, idadePai, idadeMae));
 
-/*
-        %f = ponto flutuante
-        %d = inteiro
-        %s = texto
-        %n = quebra de linha
-*/
+//        String nome;
+//        int idade, idadePai, idadeMae;
+//
+//        nome = sc.nextLine();
+//        idade = sc.nextInt();
+//        idadePai = sc.nextInt();
+//        idadeMae = sc.nextInt();
+//
+//        // Agora você pode usar as variáveis nome, idade, idadePai e idadeMae como quiser
+//        System.out.println("Nome: " + nome);
+//        System.out.println("Idade: " + idade);
+//        System.out.println("Idade do Pai: " + idadePai);
+//        System.out.println("Idade da Mãe: " + idadeMae);
+//
+//        // Não se esqueça de fechar o Scanner
+//        sc.close();
 
-        System.out.println("Products");
-        System.out.printf("Computer, which price is $ %.2f\n", price1);
-        System.out.printf("Office desk, which price is $ %.2f\n", price2);
-        System.out.println();
-        System.out.printf("Record: %d years old, code %d and gender: %s\n", age, code, gender);
-        System.out.println();
-        System.out.printf("Measure with eight decimal places: %.8f\n", measure);
-        System.out.printf("Rounded (three decimal places): %.3f\n", measure);
-
-        Locale.setDefault(Locale.US);
-        System.out.printf("US decimal point: %.3f", measure);
-
-/*
-        Products
-        Computer, which price is $ 2100,00
-        Office desk, which price is $ 650,50
-
-        Record: 30 years old, code 5290 and gender: F
-
-        Measure with eight decimal places: 53,23456700
-        Rounded (three decimal places): 53,235
-        US decimal point: 53.235
-*/
 
         sc.close();
     }
