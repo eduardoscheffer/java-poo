a253b1899739c9905b5c635bb620d3815b43d230
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-11 13:27:29 -0300

    construtores, palavra this, sobrecarga e encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,11 @@
-import introPOO.exercicioDois.entities.Product;
-
 import java.util.*;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
         Locale.setDefault((Locale.US));
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter product data:");
 
-        System.out.print("Name: ");
-        String productName = sc.nextLine();
-
-        System.out.print("Price: ");
-        double productPrice = sc.nextDouble();
-
-        System.out.print("Quantity in stock: ");
-        int productQtd = sc.nextInt();
-        System.out.println();
-
-        var product = new Product(productName, productPrice, productQtd);
-        System.out.print("Product data: " + product);
-        System.out.println();
-
-        System.out.print("Enter the number of products to be added in stock: ");
-        int qtdToBeAdded = sc.nextInt();
-        product.addProducts(qtdToBeAdded);
-        System.out.println();
-
-        System.out.print("Updated data: " + product);
-        System.out.println();
-
-        System.out.print("Enter the number of products to be removed from stock: ");
-        int qtdToBeRemoved = sc.nextInt();
-        product.removeProducts(qtdToBeRemoved);
-        System.out.println();
-
-        System.out.print("Updated data: " + product);
 
         sc.close();
     }
--- src/introPOO/exercicioStaticMembers/utils/CurrencyConverter.java
+++ src/introPOO/exercicioStaticMembers/utils/CurrencyConverter.java
@@ -1,4 +1,9 @@
 package introPOO.exercicioStaticMembers.utils;
 
 public class CurrencyConverter {
+    public static final double IOF = 0.06;
+
+    public static double dollarToReais(double dollarPrice, double amount) {
+        return amount * dollarPrice * (1 + IOF);
+    }
 }
