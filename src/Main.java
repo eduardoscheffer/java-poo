3c9f449ef769c1dc3af700de0d8277180ced241f
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-11 19:12:24 -0300

    exercicio fixacao

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,43 @@
+import contrutoresThisSobrecargaEncapsul.exercicioFixacao.entities.Account;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault((Locale.US));
         Scanner sc = new Scanner(System.in);
+        Account acc;
 
+        System.out.print("Enter account number: ");
+        Integer accNumber = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        sc.nextLine();
+        String accHolder = sc.nextLine();
+        System.out.print("Is there an initial deposit (y/n)?: ");
+        char response = sc.next().charAt(0);
+        if (response == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            acc = new Account(accNumber, accHolder, initialDeposit);
+        } else {
+            acc = new Account(accNumber, accHolder);
+        }
+        System.out.println();
 
+        System.out.println("Account data:");
+        System.out.println(acc);
+        System.out.println();
 
-        sc.close();
+        System.out.print("Enter a deposit value: ");
+        acc.deposit(sc.nextDouble());
+        System.out.println("Updated account data:");
+        System.out.println(acc);
+        System.out.println();
+
+        System.out.print("Enter a withdraw value: ");
+        acc.withdraw(sc.nextDouble());
+        System.out.println("Updated account data:");
+        System.out.println(acc);
+
     }
 }
---
+++ src/contrutoresThisSobrecargaEncapsul/exercicioFixacao/entities/Account.java
@@ -0,0 +1,45 @@
+package contrutoresThisSobrecargaEncapsul.exercicioFixacao.entities;
+
+public class Account {
+    private final Integer number;
+    private String holder;
+    private double balance;
+
+    public Integer getNumber() {
+        return this.number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String newName) {
+        this.holder = newName;
+    }
+
+    public void deposit(Double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(Double amount) {
+        balance -= (amount + 5.00);
+    }
+
+    public Account(Integer numberAcc, String holderAcc) {
+        number = numberAcc;
+        holder = holderAcc;
+    }
+
+    public Account(Integer numberAcc, String holderAcc, double initialDeposit) {
+        this(numberAcc, holderAcc);
+        deposit(initialDeposit);
+    }
+
+    @Override
+    public String toString() {
+        return "Account " + number
+                + ", Holder: "
+                + holder + ", Balance: $ "
+                + String.format("%.2f", balance);
+    }
+}
