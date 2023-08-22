2998ff702f02b406d6bc57da08a2fd45eb65dd29
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-22 13:25:16 -0300

    Exercicio resolvido 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import enumsComposicoes.exercicioWorker.entities.Department;
-import enumsComposicoes.exercicioWorker.entities.HourContract;
-import enumsComposicoes.exercicioWorker.entities.Worker;
-import enumsComposicoes.exercicioWorker.entities.enums.WorlerLevel;
+import enumsComposicoes.exercicioPost.entities.Comment;
+import enumsComposicoes.exercicioPost.entities.Post;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -13,57 +10,20 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault((Locale.US));
         Scanner sc = new Scanner(System.in);
-        DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-        System.out.print("Enter department's name: ");
-        Department workerDepartment = new Department(sc.nextLine());
+        Comment c1 = new Comment("Have a nice trip");
+        Comment c2 = new Comment("Wow that's awesome!");
+        Post p1 = new Post(LocalDate.parse("21/06/2018 13:05:44", fmt), "Traveling to New Zealand", "Im going to visit this wonderful country!", 12);
 
-        System.out.println("Enter worker data:");
-        System.out.print("Name: ");
-        String workerName = sc.nextLine();
+        Comment c3 = new Comment("Good night");
+        Comment c4 = new Comment("May the Force be with you");
+        Post p2 = new Post(LocalDate.parse("28/07/2018 23:14:19", fmt), "Good night guys", "See you tomorrow", 5);
 
-        System.out.print("Level: ");
-        WorlerLevel workerLevel = WorlerLevel.valueOf(sc.nextLine());
-
-        System.out.print("Base salary: ");
-        Double workerBaseSalary = sc.nextDouble();
-
-        Worker worker = new Worker(workerName, workerLevel, workerBaseSalary, workerDepartment);
-
-        System.out.print("How many contracts to this worker? ");
-        int n = sc.nextInt();
-
-        sc.nextLine(); // faz o consumo da quebra de linha do buffer
+        System.out.println(p1);
+        System.out.println(p2);
 
 
-        for (int i = 1; i <= n; i++) {
-
-            System.out.printf("Enter contract #%d data:\n", i);
-            System.out.print("Date (DD/MM/YYYY): ");
-            LocalDate dataContract = LocalDate.parse(sc.nextLine(), dateFmt);
-            System.out.print("Value per hour: ");
-            Double contractValuePerHour = sc.nextDouble();
-            System.out.print("Duration (hours): ");
-            Integer contractHours = sc.nextInt();
-
-            // instancia um novo HourContract e adiciona ao worker:
-            worker.addContract(new HourContract(dataContract, contractValuePerHour, contractHours));
-
-            sc.nextLine(); // faz o consumo da quebra de linha do buffer
-            System.out.println();
-
-        }
-
-        System.out.print("Enter month and year to calculate income (MM/YYYY): ");
-        // O YearMonth representa um mês e ano específicos, mas não inclui informações de hora ou dia.
-        YearMonth monthAndYear = YearMonth.parse(sc.next(), DateTimeFormatter.ofPattern("MM/yyyy"));
-
-        System.out.println("Name: " + workerName);
-        System.out.println("Department: " + workerDepartment.getName());
-        System.out.println("Income for "
-                + monthAndYear.getMonthValue()
-                + "/" + monthAndYear.getYear()
-                + ": " + String.format("%.2f", worker.income(monthAndYear.getYear(), monthAndYear.getMonthValue())));
         sc.close();
 
     }
---
+++ src/enumsComposicoes/exercicioPost/entities/Post.java
@@ -0,0 +1,80 @@
+package enumsComposicoes.exercicioPost.entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+    private LocalDate moment;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    List<Comment> comments = new ArrayList<>();
+
+    public Post() {
+    }
+
+    public Post(LocalDate moment, String title, String content, Integer likes) {
+        this.moment = moment;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public LocalDate getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDate moment) {
+        this.moment = moment;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        comments.remove(comment);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Post{");
+        sb.append("moment=").append(moment);
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", content='").append(content).append('\'');
+        sb.append(", likes=").append(likes);
+        sb.append(", comments=").append(comments);
+        sb.append('}');
+        return sb.toString();
+    }
+}
