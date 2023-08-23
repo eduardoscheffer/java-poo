977d6afa1e8f0233bde5f52182fc1658c3d6597c
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-08-23 13:24:54 -0300

    init Exercicio fixacao

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -6,7 +8,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault((Locale.US));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
+        System.out.println("Enter client data");
+        System.out.print("Name: ");
+        String clientName = sc.nextLine();
+        System.out.print("Email: ");
+        String clientEmail = sc.next();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        LocalDate clientBirthDate = LocalDate.parse(sc.next(), formatter);
+
+        System.out.println("Enter order data:");
 
         sc.close();
 
