271c294f63ebc91cfbfd9475565328494fe8477e
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-09-16 09:24:23 -0300

    finished Programacao Funcional

---
+++ src/programacaoFuncional/Stream/exercicioFixacao/entities/Employee.java
@@ -0,0 +1,14 @@
+package programacaoFuncional.Stream.exercicioFixacao.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+    private String name;
+    private String email;
+    private Double salary;
+}
