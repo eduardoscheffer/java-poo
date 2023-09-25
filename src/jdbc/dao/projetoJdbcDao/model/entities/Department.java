b3317ea936bbe03c22f87be1a4151d8a00978d6c
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-09-25 18:54:06 -0300

    projetoJdbcDao Department entity Seller

--- src/jdbc/dao/projetoJdbcDao/model/entities/Department.java
+++ src/jdbc/dao/projetoJdbcDao/model/entities/Department.java
@@ -1,8 +1,11 @@
 package jdbc.dao.projetoJdbcDao.model.entities;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Getter
 public class Department implements Serializable {
 
     private Integer id;
@@ -29,16 +32,8 @@ public class Department implements Serializable {
         return Objects.hash(id);
     }
 
-    public Integer getId() {
-        return id;
-    }
-
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
